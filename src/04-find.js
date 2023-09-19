7b953ac803cb84827150a126ddcde3b7e88699bc
lmarquez1020 <luismarquez@pursuit.org>
2023-09-19 13:27:19 -0400

    completed my assignment

--- src/04-find.js
+++ src/04-find.js
@@ -1,33 +1,29 @@
-/*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
-
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
-
-  Keep in mind that your functions must still have and use a parameter for accepting all songs.
-*/
-const exampleSongData = require("../data/songs");
-// Do not change the line above.
-
 /**
  * Returns the song "Pink Elephants" from the song array, if it is in the array.
  * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object} A single object.
+ * @returns {Object} A single object or undefined if not found.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  return songs.find((song) => song.title === "Pink Elephants" && song.artist === "Timestreet");
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object} A single object.
+ * @returns {Object} A single object or undefined if not found.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  return songs.find((song) => song.runtimeInSeconds < 180); // Assuming runtimeInSeconds is in seconds
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
- * @returns {Object} A single object.
+ * @returns {Object} A single object or undefined if not found.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  return songs.find((song) => song.title === song.album);
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
--- src/06-sort.js
+++ src/06-sort.js
@@ -1,39 +1,32 @@
-/*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
-
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
-
-  Keep in mind that your functions must still have and use a parameter for accepting all songs.
-*/
-const exampleSongData = require("../data/songs");
-// Do not change the line above.
-
 /**
  * Reorders the array so that the song objects are organized by their runtime. The shortest song should come first.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  // Sort the songs array in ascending order of runtimeInSeconds
+  return songs.slice().sort((a, b) => a.runtimeInSeconds - b.runtimeInSeconds); // Assuming runtimeInSeconds is in seconds
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
- *
- * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
- *
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  // Sort the songs array in descending order of artist names
+  return songs.slice().sort((a, b) => b.artist.localeCompare(a.artist));
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
- *
- * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
- *
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  // Sort the songs array in ascending order of song titles
+  return songs.slice().sort((a, b) => a.title.localeCompare(b.title));
+}
 
 module.exports = {
   sortByRuntimeAscending,
