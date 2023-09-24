c382beefa944afbe61285f46d651c25cf255a873
Jpena101 <jannpena@pursuit.org>
2023-09-24 01:38:55 -0400

    completed

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,11 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+// sortByRuntimeAscending.js
+
+function sortByRuntimeAscending(songs) {
+  return songs.slice().sort((a, b) => a.runtimeInSeconds - b.runtimeInSeconds);
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,8 +27,9 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
-
+function sortByArtistNameDescending(songs) {
+  return songs.slice().sort((a, b) => b.artist.localeCompare(a.artist));
+}
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
  *
@@ -33,8 +38,9 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
-
+function sortBySongTitleAscending(songs) {
+  return songs.slice().sort((a, b) => a.title.localeCompare(b.title));
+}
 module.exports = {
   sortByRuntimeAscending,
   sortByArtistNameDescending,
